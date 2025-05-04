2e8079fcc9165df8cdb0776c18c0e18d710b2081
yogeshgiri <yogeshgiri904@gmail.com>
2025-05-04 14:32:16 +0530

    minor additions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,8 +9,8 @@ plugins {
 android {
     namespace = "com.example.namaste_flutter"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
+    ndkVersion = "27.0.12077973"
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
